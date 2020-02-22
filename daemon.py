75b18f51595ffba3466561e315a92014eed082de
Valeriy Kuznetsov <vkuz.perm@gmail.com>
2020-02-22 12:15:40 +0500

    fix

--- daemon.py
+++ daemon.py
@@ -6,19 +6,18 @@ if __name__ == '__main__':
 def rabbit_callback(ch, method, properties, body):
     print(" [x] Received %r" % body.decode())
 
-    print('Starting up ...')
-    time.sleep(1)
-    print('Startup complete')
-    # Tell systemd that our service is ready
-    systemd.daemon.notify(systemd.daemon.Notification.READY)
+print('Starting up ...')
+time.sleep(1)
+print('Startup complete')
+# Tell systemd that our service is ready
+systemd.daemon.notify(systemd.daemon.Notification.READY)
 
+print('Hello from the Python rabbit pull Service')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
 
-    print('Hello from the Python rabbit pull Service')
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='hello')
-
-    channel.basic_consume(queue='hello',
-                        auto_ack=True,
-                        on_message_callback=rabbit_callback)
-    channel.start_consuming()
+channel.basic_consume(queue='hello',
+                    auto_ack=True,
+                    on_message_callback=rabbit_callback)
+channel.start_consuming()
