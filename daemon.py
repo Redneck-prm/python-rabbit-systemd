cd63a15a3089b21a6fc534a2dc94c3fb50e429be
Valeriy Kuznetsov <vkuz.perm@gmail.com>
2020-02-22 11:51:26 +0500

    fix

--- daemon.py
+++ daemon.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     time.sleep(5)
     print('Startup complete')
     # Tell systemd that our service is ready
-    systemd.daemon.notify('READY=1')
+    systemd.daemon.notify(systemd.daemon.Notification.READY)
 
     while True:
         print('Hello from the Python Demo Service')
